9be5220adc95fb4c3daeba70157567eaaadd3379
tellurium <huairenpp@gmail.com>
2012-08-18 23:50:14 +0800

    solve applet problem..

---
+++ web/asdf/src/cn/edu/shu/enisp/EnterpriseServlet.java
@@ -0,0 +1,10 @@
+package cn.edu.shu.enisp;
+
+@SuppressWarnings("serial")
+public class EnterpriseServlet extends BaseServlet {
+
+    public void processAction(String action, HttpServletRequest request, 
+            HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+}
